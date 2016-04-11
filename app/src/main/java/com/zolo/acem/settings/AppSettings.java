30a1afa3b1fae960203ac50ad395b7a583abd2ad
zolo <oregzen@gmail.com>
2016-04-11 16:45:40 +0900

    Initial commit

---
+++ app/src/main/java/com/zolo/acem/settings/AppSettings.java
@@ -0,0 +1,33 @@
+package com.zolo.acem.settings;
+
+public class AppSettings {
+
+
+    public static final boolean ENABLE_ADMOB = false;
+
+
+    public static double LATITUDE = 47.918152;
+
+
+    public static double LONGITUDE = 106.917306;
+
+
+    public static final int MAP_INITIAL_ZOOM = 14;
+
+
+    public static final int GOOGLE_PLACES_LOCATION_RADIUS =1000;
+
+
+    public static final int GOOGLE_PLACES_SEARCH_RADIUS = 2000;
+
+
+    public static final String TOWN = "Ulaanbaatar";
+
+
+    public static final String COUNTRY = "Mongolia";
+
+    public static final String OPEN_WEATHER_MAP_KEY = "2c3d347e622212aa218e115628a333e6";
+
+    public static final String XMLResourcePath = "about.xml";
+
+}
